8dbcc63bd0d5aad2b7c69fb58a7fc24a53ef76f0
Sofiia <sofiia.butkova>
2025-05-05 10:15:58 +0400

    Login functionality refactor

--- tests/e2e/e2e-payment.spec.ts
+++ tests/e2e/e2e-payment.spec.ts
@@ -3,13 +3,21 @@ import { HomePage } from '../../page-objects/HomePage'
 import { LoginPage } from '../../page-objects/LoginPage'
 
 test.describe("New payment", () => {
-    
+    let homePage: HomePage
+    let loginPage: LoginPage
+
     test.beforeEach(async ({page}) => {
-        await page.goto('http://zero.webappsecurity.com/index.html')
-        await page.click('#signin_button')
-        await page.fill('#user_login', 'username')
-        await page.fill('#user_password', 'password')
-        await page.click("text=Sign in")
+        homePage = new HomePage(page)
+    loginPage = new LoginPage(page)
+
+     await homePage.visit()
+     await homePage.clickOnSignIn()
+     await loginPage.login('username', 'password')
+        // await page.goto('http://zero.webappsecurity.com/index.html')
+        // await page.click('#signin_button')
+        // await page.fill('#user_login', 'username')
+        // await page.fill('#user_password', 'password')
+        // await page.click("text=Sign in")
     })
 
     test('Should send new payment', async ({page}) =>{
